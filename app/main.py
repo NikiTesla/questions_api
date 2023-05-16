69d1559d675871760df56da3bc8eb885db6d63b4
NikiTesla <krechetov.n@outlook.com>
2023-05-16 18:46:34 +0300

    first task solved (except documentation and tests)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from models.models import Question, save_question
 import requests
 import json
+import pickle
 
 app = FastAPI(
     title="Testing FastAPI"
@@ -17,12 +19,14 @@ def index():
     return "hello, world"
 
 @app.post("/question")
-async def question(question_request: QuestionRequest):
+async def question(question_request: QuestionRequest) -> JSONResponse:
     saved_amount = 0
-    saved_questions = []
+    last_saved_question = {}
     while saved_amount < question_request.questions_num:
+        # let's make GET request to obtain questions 
         resp = requests.get(f"https://jservice.io/api/random?count={question_request.questions_num - saved_amount}")
         if resp.status_code == 200:
+            # if everything is ok, we parse list with json objects and try to save it
             json_raw = json.loads(resp.content)
             for json_obj in json_raw:
                 question = Question(
@@ -34,8 +38,13 @@ async def question(question_request: QuestionRequest):
 
                 saved = save_question(question)
                 if saved:
-                    print("saved", saved_amount)
                     saved_amount += 1
-                    saved_questions.append(json_obj["question"])
-
-    return json.dumps(saved_questions)
+                    last_saved_question = {
+                        "id": json_obj["id"],
+                        "question": json_obj["question"],
+                        "answer": json_obj["answer"],
+                        "created_at": json_obj["created_at"]
+                    }
+    if not last_saved_question:
+        return JSONResponse({"result": {}})
+    return JSONResponse({"result": last_saved_question})
--- models/models.py
+++ models/models.py
@@ -27,7 +27,6 @@ class Question(Base):
     answer = Column(String)
     created_at = Column(DateTime)
 
-
 Session = sessionmaker(bind=engine)
 
 
